9770f6814be27df5991a092e1ae8894b761f1d3e
patch7 <patch_m@mail.ru>
2017-09-15 14:42:00 +0300

    Bug fixed

--- sliding_median.h
+++ sliding_median.h
@@ -36,7 +36,7 @@ template<typename Any> Any SlidingMedian<Any>::get()
   if(change)
   {
     std::deque<Any> d(*this);
-    std::sort(d.begin(). d.end());
+    std::sort(d.begin(), d.end());
     mediane = *(d.begin() + d.size() / 2);
   }
   return mediane;
